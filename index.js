321cb8f71d56f45a54e0b96356e91426a455c65f
Robin <robin.kriisa@voco.ee>
2023-01-04 12:56:42 +0200

    ä

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const path = require('path')
+const hbs = require('express-handlebars');
+app.set('views', path.join(__dirname,'views'))
+app.set('view engine','hbs')
+app.engine('hbs', hbs.engine({
+    extname:'hbs',
+    defaultlayout: 'main',
+    layoutsDir: __dirname +'/views/layouts'
+}))
+
+app.use(express.static(path.join(__dirname,'/public/')))
+
+app.listen(3003, () => {
+    console.log('web server is started')
+})
+
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "joga_mysql"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+});
+
+app.get('/',(req, res) => {
+    let sql = "SELECT * FROM article"
+    con.query(sql, (err,result)=> {
+        if(err) throw err;
+        console.log(result)
+        res.render('index', {
+            articles: result
+        })
+    })
+
+})
+
+app.get('/article/:slug')
+
+
