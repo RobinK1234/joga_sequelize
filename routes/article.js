6e579e061c9524921459ba78f8da2b83e7c0f6c5
Robin <robin.kriisa@voco.ee>
2023-09-15 11:28:40 +0300

    Inserted new entry into database via Postman

--- routes/article.js
+++ routes/article.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const articleController = require('../controllers/article')
 
-router.get('/', articleController.getAllArticles)
-router.get('/:slug', articleController.getArticlesBySlug)
+router.get('/', articleController.getAllArticles);
+router.get('/:slug', articleController.getArticlesBySlug);
+router.post('/create', articleController.createNewArticle);
 
 module.exports = router
